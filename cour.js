cc339ae734481cf8f4329a9ce7938c132de5b8a7
abdelmadjidSiradj <a.siradj.infg@lagh-univ.dz>
2025-12-03 15:41:12 +0100

    Add files via upload

---
+++ cour.js
@@ -0,0 +1,20 @@
+
+const display= document.getElementById("display")
+function addButton(input){
+    display.value += input
+
+} 
+function delet(input){
+    display.value = display.value.slice(0,-1)
+    display.value = display.value.slice(0,-1)
+    
+
+}
+
+function solve(input){
+    display.value = eval(display.value)
+
+}
+function cleanall(input){
+    display.value =" "
+}
